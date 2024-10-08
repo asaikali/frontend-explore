8051aaea6d9757de52094fde0a985cb7b43edf32
Adib Saikali <asaikali@gmail.com>
2024-10-07 20:06:25 -0400

    Polish

--- vue/vue/src/components/Counter.vue
+++ vue/vue/src/components/Counter.vue
@@ -1,15 +1,34 @@
 <script setup lang="ts">
 
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 const count = ref(0)
 
 function increment() {
     count.value++
 }
+
+const oldMessages = ref<string[]>([]);
+
+// computed propreties automaticaly change when the 
+// underlying reactive values changes 
+const fullMessage = computed( (previous: any) => {
+    oldMessages.value.push(previous)
+    return "This a computed property based on count which has a value of " + count.value 
+
+})
 </script>
 
 <template>
     <button @click="increment">
        Click to increment count={{ count }}
     </button>
+
+    <p> 
+        {{ fullMessage }}
+    </p>
+
+
+    <p v-if="oldMessages.length > 1"> 
+    {{ oldMessages }}
+    </p>
 </template>

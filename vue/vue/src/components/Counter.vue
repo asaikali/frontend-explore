dfc4c38c8e8b057cedcb570f01657aa10d73d6ad
Adib Saikali <asaikali@gmail.com>
2024-10-07 20:44:23 -0400

    Polish

--- vue/vue/src/components/Counter.vue
+++ vue/vue/src/components/Counter.vue
@@ -12,7 +12,8 @@ const oldMessages = ref<string[]>([]);
 // computed propreties automaticaly change when the 
 // underlying reactive values changes 
 const fullMessage = computed( (previous: any) => {
-    oldMessages.value.push(previous)
+
+   oldMessages.value[count.value-1]= previous
     return "This a computed property based on count which has a value of " + count.value 
 
 })
@@ -23,12 +24,35 @@ const fullMessage = computed( (previous: any) => {
        Click to increment count={{ count }}
     </button>
 
-    <p> 
+    <!-- the :class directive evalutes to a comma seperated list of class names,
+      based on evaluating the expressions -->
+    <p :class="{ even: count % 2 === 0, odd: count % 2 === 1 }"
+        class="count"> 
         {{ fullMessage }}
     </p>
 
 
-    <p v-if="oldMessages.length > 1"> 
-    {{ oldMessages }}
-    </p>
+    <ol v-if="oldMessages.length > 1"> 
+        <li v-for="message in oldMessages">
+            {{ message }}
+        </li>
+    </ol>
+    <p v-else>The count is 0 there are no old computed messages</p>
 </template>
+
+
+<style scoped>
+
+.count {
+    font-weight: bold;
+}
+
+.odd {
+    color: red;
+}
+
+.even {
+    color: green;
+}
+
+</style>
